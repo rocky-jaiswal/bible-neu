87c711a3fe8a124925aea025e2dbf0eb7310ec49
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2019-06-22 11:19:23 +0200

    Minor optimization.

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,26 +5,28 @@ import config from '../config';
 const TOKEN_KEY = 'token';
 class API {
 
-  async executeGQLQuery<T>(queryBuilder: Function): Promise<T> {
-    const token = await API.getToken();
-    const response = await axios
-      .post(
-        `${config.env.baseURL}/graphql`,
-        { query: queryBuilder() },
-        { headers: { Authorization: `Bearer ${token}` } }
-      );
+  static async executeGQLQuery<T>(queryBuilder: Function, retry: number = 1): Promise<T | undefined> {
+    try {
+      if (retry < 3) {
+        const token = localStorage.getItem(TOKEN_KEY);
+        const response = await axios
+          .post(
+            `${config.env.baseURL}/graphql`,
+            { query: queryBuilder() },
+            { headers: { Authorization: `Bearer ${token}` } }
+          );
 
-    return response.data.data;
-  }
-
-  private static async getToken(): Promise<String> {
-    if (!localStorage.getItem(TOKEN_KEY)) {
-      const tokenResponse = await axios.post(`${config.env.baseURL}/session`);
-      localStorage.setItem(TOKEN_KEY, tokenResponse.data.token);
+        return response.data.data;
+      }
+    } catch (err) {
+      if (err.response.status === 401) {
+        const tokenResponse = await axios.post(`${config.env.baseURL}/session`);
+        localStorage.setItem(TOKEN_KEY, tokenResponse.data.token);
+        return API.executeGQLQuery(queryBuilder, retry + 1);
+      }
     }
-    return localStorage.getItem(TOKEN_KEY)!;
   }
 
 }
 
-export default new API();
+export default API;
